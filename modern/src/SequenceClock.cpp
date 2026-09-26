5c8e63e4fe8405fe12eff84b85c11c3025b65ea7
Gosco <206429321+Giscolab@users.noreply.github.com>
2026-09-26 14:56:42 +0200

    Allow legacy sound sequences to run on sequence clock

--- modern/src/SequenceClock.cpp
+++ modern/src/SequenceClock.cpp
@@ -34,7 +34,7 @@ namespace monopoly::sequence
     {
         switch (record.chunk.id)
         {
-        case 1: case 2: case 3: case 4: case 6: case 7: case 9: case 10: break;
+        case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 9: case 10: break;
         default: return std::unexpected(ClockError::UnsupportedSequenceType);
         }
         if (record.header.scrollingWorld)
